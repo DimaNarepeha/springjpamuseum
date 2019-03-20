b2c5f12e9e8fcabc36227b233c41502e846a152a
Anast <nastya_strong@ukr.net>
2019-03-20 16:17:21 +0200

    initial commit

---
+++ src/main/java/ua/home/config/WebConfig.java
@@ -0,0 +1,18 @@
+package ua.home.config;
+
+import ua.home.service.GameService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "ua.home")
+public class WebConfig {
+
+    @Bean
+    public GameService gameService() {
+        return new GameService();
+    }
+}
