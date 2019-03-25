d4f02b587eb15e1f1773f66efefa80c068f54ce1
Anast <nastya_strong@ukr.net>
2019-03-25 15:08:54 +0200

    add tests

--- src/test/java/ua/home/service/TestServiceTest.java
+++ src/test/java/ua/home/service/TestServiceTest.java
@@ -22,12 +22,20 @@ import static org.junit.Assert.*;
 public class TestServiceTest {
     @Rule
     public ExpectedException exceptionRule = ExpectedException.none();
-@Autowired
-TestService testService;
-    @Test
-    public void whenExceptionThrown_thenAssertionSucceeds() {
-        exceptionRule.expect(NotFoundException.class);
-        exceptionRule.expectMessage("No such element in database!!!");
-        testService.get(1);
-}
+    @Autowired
+    TestService testService;
+   /* @Test
+    public void whenEnteringNumber_thenNumberIsStored() {
+        testService.save(2222);
+        assertTrue(testService.get(1).getNumber()==2222);
+    }*/
+        @Test
+        public void whenExceptionThrown_thenAssertionSucceeds() {
+            exceptionRule.expect(NotFoundException.class);
+            exceptionRule.expectMessage("No such element in database!!!");
+            testService.get(2);
+    }
+
+
+
 }
